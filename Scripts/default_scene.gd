42b820311f9be43da4af91dc7c37938ac4780a63
Mark <markarneman@gmail.com>
2025-02-09 14:13:53 -0500

    code cleanup

--- Scripts/default_scene.gd
+++ Scripts/default_scene.gd
@@ -21,10 +21,3 @@ func setup_scene(entity: OpenXRFbSpatialEntity) -> void:
 	var collision_shape = entity.create_collision_shape()
 	if collision_shape:
 		add_child(collision_shape)
-
-	#var mesh_instance = entity.create_mesh_instance()
-	#if mesh_instance:
-		#add_child(mesh_instance)
-		#var material = StandardMaterial3D.new()
-		#material.albedo_texture = sub_viewport.get_texture()
-		#mesh_instance.set_surface_override_material(0, material)
